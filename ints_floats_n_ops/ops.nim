520339cf3e274549518704c364aa8f1511a65657
Prabhakar Dev <ayushashi11@gmail.com>
2020-07-27 09:20:54 +0530

    .

--- ints_floats_n_ops/ops.nim
+++ ints_floats_n_ops/ops.nim
@@ -5,6 +5,6 @@ echo "with ",x," and ",y
 echo "addition gives ",x+y
 echo "subtraction gives ",x-y
 echo "multiplication gives ",x*y
-echo "division gives quotient ",x/y
+echo "division gives ",x/y
 echo "division gives remainder ",x mod y
 echo "floor division gives ",x div y
