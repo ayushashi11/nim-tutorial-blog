da9644b646e80d63bd4627f1d89cff9cbfedc39d
Prabhakar dev <ayushashi11@gmail.com>
2020-09-25 12:27:03 +0530

    forloops 4

--- forloops/src/forloops2.nim
+++ forloops/src/forloops2.nim
@@ -2,3 +2,4 @@ let st = "I am a string."
 var length = 0
 for _ in st:
     length.inc
+echo "lentgh of the string '"&st&"' is "&($length)
--- forloops/src/forloops3.nim
+++ forloops/src/forloops3.nim
@@ -2,4 +2,6 @@ let st = "The quick brown fox jumped over the lazy dog"
 var count = 0
 for ch in st:
     if ch == 'o':
+        echo "found one 'o'"
         count += 1
+echo "there are "&($count)&" 'o's in the sentence: '"&st&"'"
