e7d44cc74bf140286a9541486beb8adcfafc69ed
Prabhakar dev <ayushashi11@gmail.com>
2021-01-16 22:54:38 +0530

    blocks2

---
+++ blocks/block1.nim
@@ -0,0 +1,9 @@
+const NIMISGREAT = true
+if NIMISGREAT:
+    echo "Nim is Great"
+
+while not NIMISGREAT:
+    echo "Nim should become better"
+
+block:
+    echo "the block says a value NIMISGREAT=", NIMISGREAT
